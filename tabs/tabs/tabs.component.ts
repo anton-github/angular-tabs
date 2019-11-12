5cf57dae140485e8008de4e2378142e88d78039d
Anton Timofeenko <shockfront00@gmail.com>
2019-11-13 00:58:32 +0500

    use proper hook method

--- tabs/tabs/tabs.component.ts
+++ tabs/tabs/tabs.component.ts
@@ -1,27 +1,51 @@
-import {AfterViewInit, Component, ContentChild, ContentChildren, OnInit, QueryList, ViewChildren} from '@angular/core';
+import {AfterContentInit, AfterViewInit, Component, ContentChildren, QueryList, ViewChildren} from '@angular/core';
 import {TabComponent} from "../tab/tab.component";
-import {TabTitleComponent} from "../tab-title/tab-title.component";
-import {TabContentComponent} from "../tab-content/tab-content.component";
 
 @Component({
     selector: 'tabs',
     templateUrl: './tabs.component.html',
     styleUrls: ['./tabs.component.scss']
 })
-export class TabsComponent implements AfterViewInit {
+export class TabsComponent implements AfterContentInit {
 
     @ContentChildren(TabComponent) tabs: QueryList<TabComponent>;
     activeTab: TabComponent = null;
 
     constructor() {
+        this.onTabsChanged = this.onTabsChanged.bind(this);
     }
 
-    ngAfterViewInit(): void {
-        setTimeout(() => {
-            if (this.tabs.first) {
-                this.activeTab = this.tabs.first
+    ngAfterContentInit() {
+        this.initTabs();
+    }
+
+    private setInitialTab() {
+        if (this.tabs.first) {
+            this.activeTab = this.tabs.first;
+        }
+    }
+
+    private setTabOnRemoveActive() {
+        if (this.tabs.first) {
+            this.activeTab = this.tabs.first;
+        } else {
+            this.activeTab = null;
+        }
+    }
+
+    private initTabs() {
+        this.setInitialTab();
+        this.tabs.changes.subscribe(this.onTabsChanged)
+    }
+
+    private onTabsChanged() {
+        if (this.activeTab) {
+            if (this.tabs.find((tab => tab === this.activeTab)) === undefined) {
+                this.setTabOnRemoveActive();
             }
-        })
+        } else {
+            this.setInitialTab();
+        }
     }
 
     activateTab(tab) {
